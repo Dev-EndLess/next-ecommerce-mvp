57e314c6d9674d310258ab2abd7e14a428125f22
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-07-15 17:32:56 +0200

    added Zod Schema

--- src/app/admin/actions/Products.ts
+++ src/app/admin/actions/Products.ts
@@ -1,4 +1,54 @@
-"use server"
+"use server";
+import db from "@/db/db";
+import { z } from "zod";
+import fs from "fs/promises";
+import { redirect } from "next/navigation";
+
+const fileSchema = z.instanceof(File, { message: "File is required" });
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/"),
+  {
+    message: "Image is required",
+  }
+);
+
+const addSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  priceInCents: z.coerce.number().int().min(1),
+  file: fileSchema.refine((file) => file.size > 0, {
+    message: "File is required",
+  }),
+  image: imageSchema.refine((file) => file.size > 0, {
+    message: "Image is required",
+  }),
+});
+
 export async function AddProduct(formData: FormData) {
-  console.log(formData)
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
+
+  await fs.mkdir("products", { recursive: true });
+  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+
+  await fs.mkdir("public/products", { recursive: true });
+  const imagePath = `products/${crypto.randomUUID()}-${data.image.name}`;
+  await fs.writeFile(`public/${imagePath}`, Buffer.from(await data.image.arrayBuffer()));
+
+  await db.product.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath,
+      imagePath
+    },
+  });
+
+  redirect("/admin/products");
 }
