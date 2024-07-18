ad34ca7986ec53d2b9b30cd6970510a6abc983b5
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-07-18 09:04:23 +0200

    update db

--- prisma/migrations/20240714062130_init/migration.sql
+++ prisma/migrations/20240714062130_init/migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE "Product" (
     "imagePath" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "isAvailableForPurchase" BOOLEAN NOT NULL DEFAULT true,
-    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
 
