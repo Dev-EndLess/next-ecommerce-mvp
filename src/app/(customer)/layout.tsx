4b774183d85fb2a2774ac29b62319a35d8b1f40f
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-07-18 09:05:35 +0200

    customer home page

---
+++ src/app/(customer)/layout.tsx
@@ -0,0 +1,20 @@
+import { Nav, NavLink } from "@/components/Nav";
+
+export const dynamic = "force-dynamic"
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/home">Home</NavLink>
+        <NavLink href="/products">My Products</NavLink>
+        <NavLink href="/orders">My Orders</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+}
---
+++ src/app/(customer)/page.tsx
@@ -0,0 +1,86 @@
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import { Button } from "@/components/ui/button";
+import db from "@/db/db";
+import { Product } from "@prisma/client";
+import { Separator } from "@radix-ui/react-dropdown-menu";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { Suspense } from "react";
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { createdAt: "desc" },
+    take: 5,
+  });
+}
+
+function getMostPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: "desc" } },
+    take: 5,
+  });
+}
+
+export default function HomePage() {
+  return (
+    <main>
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+      <Separator className="my-8" />
+      <ProductGridSection
+        title="Most Popular"
+        productsFetcher={getMostPopularProducts}
+      />
+    </main>
+  );
+}
+
+type ProductGridSectionProps = {
+  title: string;
+  productsFetcher: () => Promise<Product[]>;
+};
+
+function ProductGridSection({
+  title,
+  productsFetcher,
+}: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products" className="space-x-2">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductSuspense productsFetcher={productsFetcher} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>;
+}) {
+  {
+    return (await productsFetcher()).map((product) => (
+      <ProductCard key={product.id} {...product} />
+    ));
+  }
+}
